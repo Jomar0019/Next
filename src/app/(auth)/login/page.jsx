3349f3d7a2ebccee4b33671629d7680ad6cbfbc9
Jomar0019 <jomarmagpoot24@gmail.com>
2024-04-28 19:04:33 +0800

    Auth

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,24 @@
-const LoginPage = () => {
-    return <div>
-      <h1>LoginPage</h1>
-    </div>;
+import { auth, signIn } from "@/lib/auth";
+
+const LoginPage = async () => {
+
+  const session = await auth();
+
+  console.log(session);
+
+  const handleGithubLogin = async (e) => {
+    "use server"
+    await signIn("github");
+  }
+
+
+    return (
+      <div>
+        <form action={handleGithubLogin}>
+          <button className="bg-white p-2 rounded">Login with GitHub</button>
+        </form>
+      </div>
+    )
   };
   
   export default LoginPage;
